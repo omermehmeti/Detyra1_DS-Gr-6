9888356b6cbe614ed00f5302cfdf3a342bf9f27b
Omer Mehmeti <58736018+omermehmeti@users.noreply.github.com>
2020-04-22 00:31:43 +0200

    Update Program.cs

--- ds/Program.cs
+++ ds/Program.cs
@@ -9,6 +9,67 @@ namespace ds
     {
         static void Main(string[] args)
         {
+                        if (args.Length == 0)
+            {
+                Class1.shfaq();
+                foursquare.encrypt("kosova", "k", "oma");
+                foursquare.Decrypt("hpltvo", "k", "oma");
+            }
+            else
+            {
+
+                
+                switch (args[0])
+                {
+                    case "case":
+                        Class1.Kontrollo(args[1], args[2]);
+                        break;
+                    case "vigenere":
+                        if (args[1] == "encrypt")
+                        {
+                          Console.WriteLine(program2.Encrypt(args[2], args[3]));
+                        }
+                        else if (args[1] == "decrypt")
+                        {
+                          Console.WriteLine(program2.Decrypt(args[2], args[3]));
+                        }
+                        else
+                        {
+                            Console.WriteLine("keni shtypur dicka gabim");
+                            Class1.shfaq();
+                        }
+                        break;
+                    case "foursquare":
+                        if (args[1] == "encrypt")
+                        {
+                            foursquare.encrypt(args[2],args[3],args[4]);
+                        }
+                        else if (args[1] == "decrypt")
+                        {
+                             foursquare.Decrypt(args[2],args[3],args[4]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("keni shtypur dicka gabim");
+                            
+                            Class1.shfaq();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("operacioni qe keni kerkuar nuk mund te mundesohet");
+                        Class1.shfaq();
+                        break;
+                }
+                
+
+
+
+
+
+
+
+
+            }
             
           
 
