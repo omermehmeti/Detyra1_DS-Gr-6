2263dc287afef1208d0e549d8d28f8612cf350f6
Omer Mehmeti <58736018+omermehmeti@users.noreply.github.com>
2020-04-30 12:27:03 +0200

    exit codes in case of wrong args

--- ds/Program.cs
+++ ds/Program.cs
@@ -29,7 +29,7 @@ namespace ds
                     case "case":
                         Class1.Kontrollo(args[1], args[2]);
                         return 0;
-                        break;
+                        
                     case "vigenere":
                         if (args[1] == "encrypt")
                         {
@@ -47,7 +47,7 @@ namespace ds
                             Class1.shfaq();
                             return 1;
                         }
-                        break;
+                        
                     case "foursquare":
                         if (args[1] == "encrypt")
                         {
@@ -67,7 +67,7 @@ namespace ds
                             return 1;
                             
                         }
-                        break;
+                        
                      case "create-user":
                         Regex obj = new Regex("^[a-zA-Z0-9/_]*$");
                        // string s = Console.ReadLine();
@@ -88,41 +88,49 @@ namespace ds
                              Class1.Createuser(args[1]);
                                 return 0;
                         }
-                        break;
+                        
                     case "delete-user":
                         Class1.Deleteuser(args[1]);
                         return 0;
-                        break;
+                        
                     case "export-key":
                         if (args[1] == "public")
                         {
                             Class1.exportKey("public", args[2], args[3]);
+                            return 0; 
+                            
                         }
                         else if (args[1] == "private")
                         {
                             Class1.exportKey("private", args[2], args[3]);
+                            return 0;
                         }
                         else
+                        {
                             Console.WriteLine("operacioni qe kerkuat nuk mund te mundesohet");
-                        break;
+                            return 0;
+                        }
+                            
+                        
                     case "import-key":
                         Class1.importKey(args[1], args[2]);
-                        break;
+                        return 0;
+                        
                     case "write-message":
                        // Class1.writeMessage(args[1], args[2], args[3]);
-                        Console.Writeline("nuk eshte implementuar ende kjo pjes");
+                        Console.WriteLine("nuk eshte implementuar ende kjo pjes");
                         return 0;
-                        break;
+                        
                     case "read-message":
-                        Console.Writeline("//");
+                        Console.WriteLine("//");
                         return 0;
-                        break;
+                        
                         
                     default:
                         Console.WriteLine("operacioni qe keni kerkuar nuk mund te mundesohet");
                         Class1.shfaq();
                         return 1;
-                        break;
+                        
                 }
                 
 
