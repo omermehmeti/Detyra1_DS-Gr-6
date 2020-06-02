9c67288073b0a963115570b073d7baa3b0367b3c
Omer Mehmeti <58736018+omermehmeti@users.noreply.github.com>
2020-06-02 13:24:48 +0200

    Update Program.cs

--- ds/Program.cs
+++ ds/Program.cs
@@ -11,14 +11,24 @@ namespace ds
 {
     class Program
     {
-        static int Main(string[] args)
+        public static int Main(string[] args)
         {
             if (args.Length == 0)
             {
-                Class1.shfaq();/*
-                foursquare.encrypt("kosova", "k", "oma");
-                foursquare.Decrypt("hpltvo", "k", "oma");*/
-                return 0;
+                
+                Class1.shfaq();
+                return 1;
+               
+                
+
+
+
+
+
+
+
+
+
             }
             else
             {
@@ -27,11 +37,29 @@ namespace ds
                 switch (args[0])
                 {
                     case "case":
-                        Class1.Kontrollo(args[1], args[2]);
-                        return 0;
+                        if (args.Length == 3)
+                        {
+                            Class1.Kontrollo(args[1], args[2]);
+                            return 0;
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("keni shtypur dicka gabim");
+                            Class1.shfaq();
+                            return 1;
+                        }
+                           
                         
+                      
                     case "vigenere":
-                        if (args[1] == "encrypt")
+                        if (args.Length < 4)
+                        {
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
+                            return 1;
+                        }
+                        else if (args[1] == "encrypt")
                         {
                           Console.WriteLine(program2.Encrypt(args[2], args[3]));
                             return 0;
@@ -49,7 +77,13 @@ namespace ds
                         }
                         
                     case "foursquare":
-                        if (args[1] == "encrypt")
+                        if (args.Length<5)
+                        {
+                            Console.WriteLine("Keni shtypur cicka gabim\n");
+                            Class1.shfaq();
+                            return 1;
+                        }
+                        else if (args[1] == "encrypt")
                         {
                             foursquare.encrypt(args[2],args[3],args[4]);
                             return 0;
@@ -65,40 +99,64 @@ namespace ds
                             
                             Class1.shfaq();
                             return 1;
-                            
                         }
                         
-                     case "create-user":
-                        Regex obj = new Regex("^[a-zA-Z0-9/_]*$");
-                       // string s = Console.ReadLine();
-                        string u = "C:\\keys\\" + args[1] + ".xml";
-                        if (!obj.IsMatch(args[1])){
-                            Console.WriteLine("Useri  nuk mund te permbaje disa nga karakteret" +
-                                "qe keni perdorur");
-                            return 1;
-                            
-                        }
-                        else if(File.Exists(u))
+                    case "create-user":
+                       
+                        if (args.Length < 2)
                         {
-                            Console.WriteLine("Celesi " + args[1] + " ekziston paraprakisht");
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
                             return 1;
-                        }    
+                        }
                         else
                         {
-                             Class1.Createuser(args[1]);
-                                return 0;
+                            Regex obj = new Regex("^[a-zA-Z0-9/_]*$");
+                            // string s = Console.ReadLine();
+                            string u = "C:\\keys\\" + args[1] + ".xml";
+                            if (!obj.IsMatch(args[1]))
+                            {
+                                Console.WriteLine("Useri  nuk mund te permbaje disa nga karakteret" +
+                                    "qe keni perdorur");
+
+                            }
+                            else if (File.Exists(u))
+                                Console.WriteLine("Celesi " + args[1] + " ekziston paraprakisht");
+                            else
+                                Class1.Createuser(args[1]);
+                            return 0;
                         }
                         
+
+                        
+                        
                     case "delete-user":
-                        Class1.Deleteuser(args[1]);
-                        return 0;
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
+                            return 1;
+                        }
+                        else
+                        {
+                            Class1.Deleteuser(args[1]);
+                            return 0;
+
+                        }
+                            
                         
                     case "export-key":
-                        if (args[1] == "public")
+                        if (args.Length < 4)
+                        {
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
+                            return 1;
+
+                        }
+                        else if (args[1] == "public")
                         {
                             Class1.exportKey("public", args[2], args[3]);
-                            return 0; 
-                            
+                            return 0;
                         }
                         else if (args[1] == "private")
                         {
@@ -108,24 +166,61 @@ namespace ds
                         else
                         {
                             Console.WriteLine("operacioni qe kerkuat nuk mund te mundesohet");
-                            return 0;
+                            return 1;
                         }
-                            
+                           
                         
                     case "import-key":
-                        Class1.importKey(args[1], args[2]);
-                        return 0;
+                        if (args.Length < 3)
+                        {
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
+                            return 1;
+                        }
+                        else
+                        {
+                            Class1.importKey(args[1], args[2]);
+                            return 0;
+                        }
+                           
                         
                     case "write-message":
-                       // Class1.writeMessage(args[1], args[2], args[3]);
-                        Console.WriteLine("nuk eshte implementuar ende kjo pjes");
-                        return 0;
+                        if (args.Length <3)
+                        {
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
+                            return 1;
+                            //Class1.writeMessage(args[1], args[2], "");
+                        }
+                        else if(args.Length==3)
+                        {
+                            Class1.writeMessage(args[1], args[2], "");
+                            return 0;
+                        }
+                        else
+                        {
+                            Class1.writeMessage(args[1], args[2], args[3]);
+                            return 0;
+                        }
                         
                     case "read-message":
-                        Console.WriteLine("//");
-                        return 0;
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("Argumentet nuk jane ne rregull\n");
+                            Class1.shfaq();
+                            return 1;
+                        }
+                        else
+                        {
+                            Class1.readMessage(args[1]);
+                            return 0;
+                        }
+
+
+                        //Class1.readMessage(args[1]);
                         
-                        
+
+
                     default:
                         Console.WriteLine("operacioni qe keni kerkuar nuk mund te mundesohet");
                         Class1.shfaq();
@@ -142,9 +237,6 @@ namespace ds
 
 
             }
-            
-          
-
         }
     }
 }
